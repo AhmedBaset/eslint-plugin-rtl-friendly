c2f1d35630d0926a4d6939218e4cf07896581bfd
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2023-08-23 16:14:26 +0300

    config: tailwind logical classes

---
+++ src/configs/tw-logical-properties.ts
@@ -0,0 +1,50 @@
+export const logicalProperties = [
+  {
+    physical: "ml-",
+    logical: "ms-",
+  },
+  {
+    physical: "mr-",
+    logical: "me-",
+  },
+  {
+    physical: "pl-",
+    logical: "ps-",
+  },
+  {
+    physical: "pr-",
+    logical: "pe-",
+  },
+  {
+    physical: "left-",
+    logical: "start-",
+  },
+  {
+    physical: "right-",
+    logical: "end-",
+  },
+  {
+    physical: "text-left",
+    logical: "text-start",
+  },
+  {
+    physical: "text-right",
+    logical: "text-end",
+  },
+  {
+    physical: "border-l-",
+    logical: "border-s-",
+  },
+  {
+    physical: "border-r-",
+    logical: "border-e-",
+  },
+  {
+    physical: "rounded-l-",
+    logical: "rounded-s-",
+  },
+  {
+    physical: "rounded-r-",
+    logical: "rounded-e-",
+  }
+]
