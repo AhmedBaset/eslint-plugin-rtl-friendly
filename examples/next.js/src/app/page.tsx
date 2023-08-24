ceb574fe1682dbd331d0600e67a3777c433d2355
A7med3bdulBaset <A7med3bdulBaset@gmail.com>
2023-08-24 19:50:47 +0300

    refactor(example): simplfied example page

--- examples/next.js/src/app/page.tsx
+++ examples/next.js/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="ms-2 pe-4">Fancy Title</h1>
+    <main className="p-24">
+      <h1 className="ms-2 pe-4">Fancy Heading</h1>
     </main>
   );
 }
