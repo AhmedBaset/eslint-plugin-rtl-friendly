a3663eea84bab45c940541a8e4675c6fe28af27f
Ahmed Abdelbaset <a7med3bdulbaset@gmail.com>
2024-08-04 01:00:07 +0300

    Remove unused scripts

--- scripts/camilize.ts
+++
@@ -1,5 +0,0 @@
-export function camelize(str: string) {
-  return str.replace(/-([a-z])/g, (_, w: string) => {
-    return w.toUpperCase();
-  });
-}
--- scripts/update.ts
+++
@@ -1,4 +0,0 @@
-import("./lib/update-docs-headers");
-import("./lib/update-lib-configs-recommended");
-import("./lib/update-lib-index");
-import("./lib/update-readme");
